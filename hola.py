a2767ba45a7c9db9f95502055f58135fa04dc2e2
JaiderM777M <115684909+JaiderM777M@users.noreply.github.com>
2025-11-13 16:04:58 -0500

    Add meme dictionary and user input prompt

---
+++ hola.py
@@ -0,0 +1,6 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "IDK": "nose en ingles"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
